44533ff5172b87fcd5f892df6e41a238e13ae483
Rehan2205 <razarehan1999@gmail.com>
2024-07-31 00:18:33 +0530

    Updated logo and others features

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <img src={assets.LogoZ} alt="" />
+            <p>Zaykaa is an ultimate destination for fast, fresh, and delicious food delivery right at your doorstep.</p>
+            {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
@@ -20,15 +21,15 @@ const Footer = () => {
             <ul>
                 <li>Home</li>
                 <li>About us</li>
-                <li>Delivery</li>
+                {/* <li>Delivery</li> */}
                 <li>Privacy policy</li>
             </ul>
         </div>
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91-11111 22222</li>
+                <li>contact@zaykaa.com</li>
             </ul>
         </div>
       </div>
